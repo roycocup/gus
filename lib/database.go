944377c8117c865802253cc220b03a3aa843e19a
Rodrigo Dias <rodrigo.dias@browsergroup.com>
2018-07-31 13:41:53 +0100

    first blud

---
+++ lib/database.go
@@ -0,0 +1,145 @@
+package lib
+
+import (
+	"database/sql"
+	"os"
+	"github.com/sirupsen/logrus"
+)
+
+
+type DB struct {
+	TableName string
+	DBName string
+	Conn *sql.DB
+}
+
+func (client *DB) Connect(){
+	db, err := sql.Open("sqlite3", client.DBName)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	client.Conn = db
+}
+
+func (client *DB) CreateDb() {
+
+	os.Remove(client.DBName)
+
+	db, err := sql.Open("sqlite3", client.DBName)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := "create table " + client.TableName + "(id integer not null primary key, original_url mediumtext, shorten_url tinytext); delete from " + client.TableName + "; "
+
+	logrus.Info("Creating table")
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		logrus.Fatal("%q: %s\n", err, sqlStmt)
+	}
+
+}
+
+func (client *DB) Save(sql string){
+	_, err := client.Conn.Exec(sql)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func (client *DB) QueryDb(sql string) string {
+
+	stmt, err := client.Conn.Prepare(sql)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer stmt.Close()
+	var url string
+	err = stmt.QueryRow().Scan(&url)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	return url
+}
+
+
+/**
+tx, err := db.Begin()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer stmt.Close()
+	for i := 0; i < 100; i++ {
+		_, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	tx.Commit()
+
+	rows, err := db.Query("select id, name from foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err = db.Prepare("select name from foo where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var name string
+	err = stmt.QueryRow("3").Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(name)
+
+	_, err = db.Exec("delete from foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = db.Query("select id, name from foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+ */
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/siddontang/go/log"
+	"./lib"
+	"github.com/sirupsen/logrus"
+	"crypto/sha256"
+	"encoding/base64"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const DBNAME = "./shorten.db"
+const TBLNAME = "shorten"
+const DOMAIN = "bus.com"
+
+var db *lib.DB
+
+func main() {
+
+	// start db
+	db = &lib.DB{
+		DBName:DBNAME,
+		TableName:TBLNAME,
+	}
+	db.Connect()
+	db.CreateDb()
+
+	// start server and listen for get
+	startServer()
+
+	// process and send back response with html and new url
+
+}
+
+func startServer() {
+	log.Info("Starting server")
+	http.HandleFunc("/process", returnShortenURL)
+	http.HandleFunc("/", showHomePage)
+
+	// we can wrap this in a log since it should never
+	// leave the loop unless theres an error or its about to stop
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func readFromFile(filename string) (file *os.File) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0777)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return file
+}
+
+func showHomePage(w http.ResponseWriter, r *http.Request) {
+	//form := readFromFile("form.html")
+	//logrus.Info("Handling homepage")
+	w.Write([]byte("this is a sentence"))
+}
+
+func returnShortenURL(w http.ResponseWriter, r *http.Request) {
+	originalURL := r.URL.Query().Get("s")
+	encoded := makeShort(originalURL)
+	encoded = encoded[:5]
+	fullURL := "http://" + encoded + "." + DOMAIN
+	cached := db.QueryDb("select * from " + TBLNAME + " where original_url = " + originalURL)
+	db.Save("insert into " + TBLNAME + "(original_url, shorten_url) values(1, '"+originalURL+"'), (2, '"+fullURL+"')")
+	logrus.Info(cached)
+	w.Write([]byte(fullURL))
+}
+
+func makeShort(originalURL string) string {
+	logrus.Info(originalURL)
+	hasher := sha256.New()
+	hasher.Reset()
+	hasher.Write([]byte(originalURL))
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	return sha
+}
+
+
